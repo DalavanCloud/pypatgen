1b66f9513b6e5a504f2358d5459cb825d598f46d
Nathanael Davison <natemail@mail.com>
2017-05-20 16:34:50 +0100

    Exploring Space and Using Heuristic

--- optimise_level.py
+++ optimise_level.py
@@ -1,50 +1,70 @@
 #!/bin/python
+from patgen.project import Project
+from patgen.range import Range
+from patgen.selector import Selector
+
 
 def get_heuristic(b, g, r, t, hfunc):
-    false, misses = # execute with parameters, output heuristic
-    return hfunc(false, misses)
+    p = Project.load('bds')
+    d = p.dictionary.clone()
+    s = Selector(g, b, t)
+    range = Range(1, r)
+    p.train_new_layer(range, s)
+    false = p.false
+    missed = p.missed
+
+    p.dictionary = d  # do we need this?
+    return hfunc(false, missed)
 
 
 # returns best (b, g r)
 def optimise_level(prev_b, prev_g, prev_r, t, hfunc):
     bgrs = set()
     i = 0
-    while(i < 1000): # loop until convergence
-        best_h = get_heuristic(prev_b, prev_g, prev_r, t, hfunc)
 
-        next_b = prev_b
-        next_g = prev_g
-        next_r = prev_r
+    best_h = get_heuristic(prev_b, prev_g, prev_r, t, hfunc)
+    next_b = prev_b
+    next_g = prev_g
+    next_r = prev_r
+    while(i < 1000):  # loop until convergence
+        print "Starting new loop {}".format(i)
 
-        for r in xrange(max(0, prev_r - 1), prev_r + 4):# lower inclusive top exclusive
+        for r in xrange(max(0, prev_r - 1), prev_r + 2):
             if r == prev_r and next_g == prev_g and next_b == prev_b:
                 continue
+
+            print "Trying: r {}, g {}, b {}".format(r, next_g, next_b)
             curr_h = get_heuristic(next_b, next_g, r, t, hfunc)
             if curr_h < best_h:
                 best_h = curr_h
                 next_r = r
+        print "Best: r {}, g {}, b {}. best_h {}".format(next_r, next_g, next_b, best_h)
 
-        for g in xrange(max(0, prev_g - 1), prev_g + 4):# lower inclusive top exclusive
+        for g in xrange(max(0, prev_g - 1), prev_g + 2):
             if next_r == prev_r and g == prev_g and next_b == prev_b:
                 continue
+
+            print "Trying: r {}, g {}, b {}".format(next_r, g, next_b)
             curr_h = get_heuristic(next_b, g, next_r, t, hfunc)
             if curr_h < best_h:
                 best_h = curr_h
                 next_g = g
-
-        for b in xrange(max(0, prev_b - 1), prev_b + 4):# lower inclusive top exclusive
+        print "Best: r {}, g {}, b {}. best_h {}".format(next_r, next_g, next_b, best_h)
+        for b in xrange(max(0, prev_b - 1), prev_b + 2):
             if next_r == prev_r and next_g == prev_g and b == prev_b:
                 continue
+
+            print "Trying: r {}, g {}, b {}".format(next_r, next_g, b)
             curr_h = get_heuristic(b, next_g, next_r, t, hfunc)
             if curr_h < best_h:
                 best_h = curr_h
                 next_b = b
+        print "Best: r {}, g {}, b {}. best_h {}".format(next_r, next_g, next_b, best_h)
 
         if (next_b, next_g, next_r) in bgrs:
             return (next_b, next_g, next_r)
-        
         bgrs.add((next_b, next_g, next_r))
-        i++
+        i += 1
 
     print "1000 Iterations Reach (EPIC FAIL)"
     return (prev_b, prev_g, prev_r)
--- optimise_level_driver.py
+++ optimise_level_driver.py
@@ -1,22 +1,37 @@
 #!/bin/python
+import optimise_level
+
+from patgen.project import Project
+from patgen.range import Range
+from patgen.selector import Selector
 
 
 def hfunc_odd(false, misses):
-    return false + misses
+    return false + 10 * misses
 
 
 def hfunc_even(false, misses):
-    return false + misses
+    return 10 * false + misses
 
 
-def optimise_level_driver(b, g, r, t, hfunc_odd, hfunc_even):
+def optimise_level_driver(b, g, r, t, hfunc_o, hfunc_e):
     level_bgr_map = dict()
     for i in xrange(1, 10):
         if i % 2 == 0:
-            b, g, r = optimise_level(b, g, r, t, hfunc_even)
+            b, g, r = optimise_level.optimise_level(b, g, r, t, hfunc_e)
         else:
-            b, g, r = optimise_level(b, g, r, t, hfunc_odd)
+            b, g, r = optimise_level.optimise_level(b, g, r, t, hfunc_o)
 
+        print (b, g, r)
         level_bgr_map[i] = (b, g, r)
 
+        p = Project.load('bds')
+        s = Selector(g, b, t)
+        range = Range(1, r)
+        l = p.train_new_layer(range, s)
+        p.commit(l)
+        print "Commiting Layer"
     return level_bgr_map
+
+if __name__ == '__main__':
+    optimise_level_driver(3, 3, 3, 20, hfunc_odd, hfunc_even)
